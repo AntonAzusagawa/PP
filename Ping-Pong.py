5971f714c5e21a07b1103918f3ddd5d99523926c
Anton <166922635+AntonAzusagawa@users.noreply.github.com>
2024-05-04 16:00:32 +0300

    Update Ping-Pong.py

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -8,6 +8,14 @@ flowgame = True
 pausegame = False
 score1 = 0
 score2 = 0
+
+font.init()
+font1 = font.SysFont('Arial', 70)
+font2 = font.SysFont('Arial', 36)
+
+lose1 = font1.render('lose of the first', True, (180, 0, 0))
+lose2 = font1.render('lose of the first', True, (180, 0, 0))
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__()
@@ -33,30 +41,13 @@ class Player2(GameSprite):
         if keys_pressed[K_DOWN] and self.rect.y < 380:
             self.rect.y += self.speed
 
+        
+
 rocket1 = Player1('1337.png', 50, 250, 40, 120, 10)
 rocket2 = Player2('1488.png', 600, 250, 40, 120, 10)
 ball = GameSprite('232.png', 350, 250, 75, 75, 15)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+speed_x = 3
+speed_y = 3
 
 
 while flowgame:
@@ -71,7 +62,43 @@ while flowgame:
         rocket2.update()
         rocket2.reset() 
         ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(rocket1, ball) or sprite.collide_rect(rocket2, ball):
+            speed_x *= -1.0005
+            speed_y *= -1.0005
 
+        if ball.rect.y > 425 or ball.rect.y < 0:
+            speed_y *= -1.0005
+
+        if ball.rect.x < 0:
+            speed_x = 3
+            speed_y = 3
+            score2 += 1
+            ball.rect.x = 350
+            ball.rect.y = 250
+
+        
+        if ball.rect.x > 700:
+            speed_x = 3
+            speed_y = 3
+            score1 += 1
+            ball.rect.x = 350
+            ball.rect.y = 250
+
+        if score1 >= 10:
+            pausegame = True       
+            window.blit(lose2, (100, 200))
+        
+        if score2 >= 10:
+            pausegame = True       
+            window.blit(lose1, (100, 200))
+
+        font3 = font.SysFont('Arial', 25) 
+        scoreboard1 = font3.render(str(score1), True, (255, 255, 255))
+        window.blit(scoreboard1, (325, 20))
+        scoreboard2 = font3.render(str(score2), True, (255, 255, 255))
+        window.blit(scoreboard2, (375, 20))
 
     
     display.update()
